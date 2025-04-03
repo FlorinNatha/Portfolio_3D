baaa7fcd0206a888c02dd5a50ca36511a0f2b477
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-04-04 00:05:04 +0530

    update hero section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-
-const Button = () => {
+const Button = ({ name, isBeam = false, containerClass}) => {
   return (
-    <div>
-      burron
-    </div>
+    <button className={'btn $${containerClass}'}>
+      {isBeam && (
+        <span className="relative flex h-3 w-3">
+          <span className="btn-ping" />
+          <span className="btn-ping_dot" />
+        </span>
+        
+      )}
+    </button>
   )
 }
 
